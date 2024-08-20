c6eb91878d69cb0ed2370b78e5f8e5dfde5c65bf
stephen <sashioyas@gmail.com>
2024-08-20 21:32:35 +0300

    secondtwotwo

--- alx-react-app-props/src/components/App.jsx
+++ alx-react-app-props/src/components/App.jsx
@@ -1,16 +1,13 @@
-import React from 'react';
+import UserContext from './UserContext';
 import ProfilePage from './ProfilePage';
-import UserContext from './UserContext'; // Import UserContext
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
-    // Wrap ProfilePage with UserContext.Provider and pass userData as value
     <UserContext.Provider value={userData}>
       <ProfilePage />
     </UserContext.Provider>
   );
 }
-
 export default App;
