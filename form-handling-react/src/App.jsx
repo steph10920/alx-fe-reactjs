a03633d0d6c4e74aec6ffcc7749e6fdf8895e5cb
stephen <sashioyas@gmail.com>
2024-09-01 18:23:26 +0300

    added a new project

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import React from 'react';
-import FormikForm from './components/FormikForm';
+import FormikForm from './components/formikForm';
 
 function App() {
   const [count, setCount] = useState(0)
