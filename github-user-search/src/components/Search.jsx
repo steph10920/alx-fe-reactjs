fa2fa4e30ab23291a25ba62c3ef1ccbaac3af6b1
STEPHEN <sashioyas@gmail.com>
2024-09-20 22:26:19 +0300

    firsts recipe  project serach commit

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -20,7 +20,8 @@ function Search() {
       const data = await fetchUserData(username);  // Fetch user data from the API
       setUserData(data);  // Set the retrieved data
     } catch (error) {
-      setError("Looks like we can't find the user");  // Set the required error message
+      // Handle if GitHub API responds with an error or user is not found
+      setError("Looks like we can't find the user");  // Set the exact error message
     } finally {
       setLoading(false);  // Set loading to false after the request
     }
