316232280c893849d72c70659d60ee429c5526d5
STEPHEN <sashioyas@gmail.com>
2024-09-21 07:19:04 +0300

    firsts recipe  project serach commit

---
+++ github-user-search/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}', // Ensure that Tailwind scans all relevant files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
